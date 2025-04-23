398dd93f775c6bac3cd78d1c50951936043501f2
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-23 15:22:12 -0500

    usando mysql2

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -43,7 +43,7 @@ export const loginUser = async (req: Request, res: Response) => {
     const usuario = {
       id: user.id,
       names: user.names,
-      lastnames: user.lastNames,
+      lastnames: user.lastnames,
       document: user.document,
       username: user.username,
       email: user.email,
