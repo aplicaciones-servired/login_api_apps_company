abbb1beccf55d8e65e1c23c84422ae6b0e7814b5
llortegall <ivanortega_97@hotmail.com>
2025-04-27 16:31:17 +0000

    refactor: standardize string quotes and clean up imports in sidebar components

--- client/src/components/app.sidebar.tsx
+++ client/src/components/app.sidebar.tsx
@@ -1,74 +1,57 @@
-import * as React from "react"
-import {
-  ArrowUpCircleIcon,
-  LayoutDashboardIcon,
-  UsersIcon,
-} from "lucide-react"
+import { ArrowUpCircleIcon, LayoutDashboardIcon, UsersIcon, } from 'lucide-react'
+import { ComponentProps } from 'react'
 
-import { NavMain } from "@/components/nav-main"
-import { NavUser } from "@/components/nav-user"
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuItem } from '@/components/ui/sidebar'
+import { NavMain } from '@/components/nav-main'
+import { NavUser } from '@/components/nav-user'
 
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
   },
   navMain: [
     {
-      title: "Dashboard",
-      url: "#",
+      title: 'Dashboard',
+      url: '#',
       icon: LayoutDashboardIcon,
     },
     {
-      title: "Usuarios",
-      url: "#",
+      title: 'Usuarios',
+      url: '#',
       icon: UsersIcon,
     },
     /*
     {
-      title: "Lifecycle",
-      url: "#",
+      title: 'Lifecycle',
+      url: '#',
       icon: ListIcon,
     },
     {
-      title: "Analytics",
-      url: "#",
+      title: 'Analytics',
+      url: '#',
       icon: BarChartIcon,
     },
     {
-      title: "Projects",
-      url: "#",
+      title: 'Projects',
+      url: '#',
       icon: FolderIcon,
     }
     */
   ]
 }
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar({ ...props }: ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="offcanvas" {...props}>
+    <Sidebar collapsible='offcanvas' {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton
-              asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
-            >
-              <header>
-                <ArrowUpCircleIcon className="h-5 w-5" />
-                <span className="text-base font-semibold">Gane Multired - Servired</span>
-              </header>
-            </SidebarMenuButton>
+            <header>
+              <ArrowUpCircleIcon className='h-5 w-5' />
+              <span className='text-base font-semibold'>Gane Multired - Servired</span>
+            </header>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
