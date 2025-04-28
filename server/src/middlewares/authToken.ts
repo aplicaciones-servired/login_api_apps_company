12810b5a061cf1cd8edb296abad839e913c847f1
AplicacionesGane <aplicaciones@grupomultired.com.co>
2025-04-28 15:59:37 -0500

    create promises to verify token and add user to request express declaretion

--- server/src/middlewares/authToken.ts
+++ server/src/middlewares/authToken.ts
@@ -1,18 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
 import { JWT_SECRECT } from '../configs/envSchema';
+import { User } from '../model/user.model';
 import jwt from 'jsonwebtoken';
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+declare global {
+  namespace Express {
+    interface Request {
+      user: User;
+    }
+  }
+}
+
+const verifyToken = (token: string): Promise<User> => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, JWT_SECRECT, (err, decoded) => {
+      if (err) return reject(err);
+      resolve(decoded as User);
+    });
+  });
+};
+
+export const authenticateToken = async (req: Request, res: Response, next: NextFunction) => {
   const cookie = req.headers.cookie 
 
   if (!cookie) {
     res.status(401).json({ message: 'Unauthorized: Missing or invalid token' });
     return;
   }
-  const token = cookie.split('=')[1];
-
+  
   try {
-    jwt.verify(token, JWT_SECRECT);
+    const user = await verifyToken(cookie.split('=')[1])
+    req.user = user
     next();
   } catch (err) {
     if (err instanceof jwt.TokenExpiredError) {
