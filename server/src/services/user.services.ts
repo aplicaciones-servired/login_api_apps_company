97b5b5681ce35980160e8073324ba3dcd91cb003
AplicacionesGane <aplicaciones@grupomultired.com.co>
2025-04-29 12:35:27 -0500

    Enhance updateState service to restrict state changes based on user role

--- server/src/services/user.services.ts
+++ server/src/services/user.services.ts
@@ -74,10 +74,17 @@ export const findUserServicesById = async (id: string) => {
   return user;
 }
 
-export const updateState = async (id: string, newState: string) => {
+export const updateState = async (id: string, newState: string, document: number) => {
   const state = newState === '0' ? false : true
 
   try {
+    const updateAdm = await User.findOne({
+      attributes: ['sub_process'],
+      where: { document }
+    })
+
+    if (updateAdm?.dataValues.sub_process.toString() !== '100') throw new Error('Solo El Director de Tecnología puede cambiar estado');
+
     const userUpdate = await User.update({ state }, { where: { id } })
 
     if(userUpdate[0] === 1 ){
